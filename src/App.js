9ca37bf0fec93681832c05dd7e100777549de290
Ernesto Gilberto Valenzuela Gallardo <ernestogilberto@hotmail.com>
2021-11-10 20:54:34 -0600

    Subida de proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import google from './images/google.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <header>
+          <nav>
+            <ul className="nav-right-section">
+              <li><a href="">Gmail</a></li>
+              <li><a href="">Imágenes</a></li>
+              <li className="menu-icon"><a href=""></a></li>
+              <li className="perfil-photo"><a href=""></a></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <section className="main-logo">
+            <img src={google} alt=""/>
+          </section>
+          <section className="main-input">
+            <div className="main-input-container">
+                <span className="search-icon">
+
+                </span>
+              <input type="text"/>
+              <a className="micro-icon" href=""></a>
+            </div>
+          </section>
+          <section className="main-buttons">
+            <div>
+              <button>Buscar con Google</button>
+            </div>
+            <div>
+              <button>Me siento con suerte</button>
+            </div>
+          </section>
+        </main>
+        <footer>
+          <ul className="footer-left">
+            <li><a href="">Sobre Google</a></li>
+            <li><a href="">Publicidad</a></li>
+            <li><a href="">Negocios</a></li>
+            <li><a href="">Cómo0 funciona la Búsqueda</a></li>
+          </ul>
+          <ul className="footer-right">
+            <li><a href="">Privacidad</a></li>
+            <li><a href="">Condiciones</a></li>
+            <li><a href="">Preferencias</a></li>
+          </ul>
+
+        </footer>
+      </>
   );
 }
 
